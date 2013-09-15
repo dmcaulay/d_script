df86e42e965926d8416e582976f9aee9452e9204
Dan McAulay <dmcaulay@gmail.com>
2013-09-15 14:10:17 -0700

    add a railtie_name

--- lib/d_script/railtie.rb
+++ lib/d_script/railtie.rb
@@ -3,6 +3,8 @@ require 'rails'
 
 module DScript
   class Railtie < Rails::Railtie
+    railtie_name :d_script
+
     rake_tasks do
       load 'lib/d_script/tasks/master.rake'
       load 'lib/d_script/tasks/runner.rake'
