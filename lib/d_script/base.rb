b0e9c87e49511fd77120a426b339fa4366fb2816
Dan McAulay <dmcaulay@gmail.com>
2013-09-16 16:53:02 -0700

    set the timeout to zero instead of nil

--- lib/d_script/base.rb
+++ lib/d_script/base.rb
@@ -9,7 +9,7 @@ module DScript
 
     def initialize(name, settings)
       @events = {}
-      settings[:timeout] = nil
+      settings[:timeout] = 0
       @pub_redis = Redis.new(settings)
       @sub_redis = Redis.new(settings)
       @base_name = name
